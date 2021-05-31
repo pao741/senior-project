23b1a7e42e6b5a69555eed6cf6fff8c4c3eace70
Vye <anuvit741@gmail.com>
2021-05-31 18:53:17 +0700

    bug fix: spawn enemy doesn't move when attack
    change z value position of Room Manager

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -30,7 +30,7 @@ public class RoomManager : MonoBehaviour
             int randEnemy = Random.Range(0, enemyPrefabs.Length);
             int randSpawnPoints = Random.Range(0, spawnPoints.Length);
 
-            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawnPoints].position, transform.rotation);
+            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawnPoints].position, Quaternion.identity);
         }
 
         if (Input.GetKeyDown(KeyCode.T)) // input to summon enemy
