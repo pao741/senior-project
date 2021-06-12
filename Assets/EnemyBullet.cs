5fddb7fa1e2e4350f26cc741afc2f1a652d28248
Vye <anuvit741@gmail.com>
2021-06-12 13:25:11 +0700

    add enemy bullet animation and recreate enemy2 animation controller
    there was a bug for enemy 2 animation controller that make the animator tree shows nothing when opening the animator tree. Clicking on other animator will also show nothing

---
+++ Assets/EnemyBullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject hitEffect;
+    public float maxVelocity = 11f;
+    public int damage = 40;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity); /// set max velocity
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Player player = collision.collider.GetComponent<Player>();
+            if(player != null)
+            {
+                player.TakeDamage(damage);
+                GameObject effect = Instantiate(hitEffect, transform.position, transform.rotation);
+                Destroy(effect, 1f);
+                Destroy(gameObject);
+            }
+        }
+
+        
+
+    }
+}
